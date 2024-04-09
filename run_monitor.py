038ea2bff551d8ebef8dc3659851bc7d1d600b21
Jeffrey Aven <javen@avensolutions.com>
2024-04-09 12:56:26 +1000

    updates

--- run_monitor.py
+++ run_monitor.py
@@ -1,4 +1,4 @@
-import logging, os, requests
+import logging, os, requests, time
 from pystackql import StackQL
 
 # get environment variables
@@ -7,6 +7,7 @@ discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
 repo = os.getenv('GITHUB_REPO')
 owner = os.getenv('GITHUB_REPO_OWNER')
 formula_name = os.getenv('HOMEBREW_FORMULA_NAME')
+sleep_interval = os.getenv('SLEEP_INTERVAL')
 
 # set up logging
 log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
@@ -78,7 +79,7 @@ def main():
     """ % (owner, repo)
     run_stackql_stmt(mat_view_query)
     init_count = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_stargazers")[0]['count']
-    send_notification(f"⭐ found {init_count} initial repo stargazers for {owner}/{repo}")
+    logger.info(f"⭐ found {init_count} initial repo stargazers for {owner}/{repo}")
 
     # github repo watchers
     logger.info(f"👀 initializing materialized view for repo watchers for {owner}/{repo}...")
@@ -89,7 +90,7 @@ def main():
     """ % (owner, repo)
     run_stackql_stmt(mat_view_query)
     init_count = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_watchers")[0]['count']
-    send_notification(f"👀 found {init_count} initial repo watchers for {owner}/{repo}")
+    logger.info(f"👀 found {init_count} initial repo watchers for {owner}/{repo}")
 
     # github repo forks
     logger.info(f"🍴 initializing materialized view for repo forks for {owner}/{repo}...")
@@ -100,7 +101,7 @@ def main():
     """ % (owner, repo)
     run_stackql_stmt(mat_view_query)
     init_count = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_forks")[0]['count']
-    send_notification(f"🍴 found {init_count} initial repo forks for {owner}/{repo}")
+    logger.info(f"🍴 found {init_count} initial repo forks for {owner}/{repo}")
 
     # github repo issues
     logger.info(f"📊 initializing materialized view for repo issues for {owner}/{repo}...")
@@ -112,7 +113,7 @@ def main():
     """ % (owner, repo)
     run_stackql_stmt(mat_view_query)
     init_count = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_issues")[0]['count']
-    send_notification(f"📊 found {init_count} initial repo issues for {owner}/{repo}")
+    logger.info(f"📊 found {init_count} initial repo issues for {owner}/{repo}")
 
     # github org followers
     logger.info(f"🙋 initializing materialized view for org followers for {owner}...")
@@ -122,7 +123,7 @@ def main():
     """ % (owner)
     run_stackql_stmt(mat_view_query)
     init_count = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_org_followers")[0]['count']
-    send_notification(f"🙋 found {init_count} initial org followers for {owner}")
+    logger.info(f"🙋 found {init_count} initial org followers for {owner}")
 
     # github release assset downloads
     logger.info(f"📦 initializing materialized view for release asset downloads for {owner}/{repo}...")
@@ -139,27 +140,231 @@ def main():
     run_stackql_stmt(mat_view_query)
     asset_downloads = stackql.execute("SELECT asset_name, download_count FROM mvw_github_release_asset_downloads")
     for asset in asset_downloads:
-        send_notification(f"📦 found {asset['download_count']} initial downloads for {asset['asset_name']}")
+        logger.info(f"📦 found {asset['download_count']} initial downloads for {asset['asset_name']}")
 
     # homebrew downloads
     logger.info(f"🍺 initializing materialized view for homebrew downloads for {formula_name}...")
     mat_view_query = """
     CREATE OR REPLACE MATERIALIZED VIEW mvw_homebrew_downloads AS 
     SELECT
-    JSON_EXTRACT(JSON_EXTRACT(analytics, '$.install.30d'), '$.' || formula_name) as installs_30d,
-    JSON_EXTRACT(JSON_EXTRACT(analytics, '$.install.90d'), '$.' || formula_name) as installs_90d,
-    JSON_EXTRACT(JSON_EXTRACT(analytics, '$.install.365d'), '$.' || formula_name) as installs_365d
-    FROM
-    homebrew.formula.formula
+    JSON_EXTRACT(analytics, '$.install.30d.%s') as installs_30d,
+    JSON_EXTRACT(analytics, '$.install.90d.%s') as installs_90d,
+    JSON_EXTRACT(analytics, '$.install.365d.%s') as installs_365d
+    FROM homebrew.formula.formula
     WHERE formula_name = '%s'
-    """ % (formula_name)
+    """ % (formula_name, formula_name, formula_name, formula_name)
     run_stackql_stmt(mat_view_query)
     init_count_30d = stackql.execute("SELECT installs_30d FROM mvw_homebrew_downloads")[0]['installs_30d']
-    send_notification(f"🍺 found {init_count_30d} initial 30d downloads for {formula_name}")
+    logger.info(f"🍺 found {init_count_30d} initial 30d downloads for {formula_name}")
     init_count_90d = stackql.execute("SELECT installs_90d FROM mvw_homebrew_downloads")[0]['installs_90d']
-    send_notification(f"🍺 found {init_count_90d} initial 90d downloads for {formula_name}")
+    logger.info(f"🍺 found {init_count_90d} initial 90d downloads for {formula_name}")
     init_count_365d = stackql.execute("SELECT installs_365d FROM mvw_homebrew_downloads")[0]['installs_365d']
-    send_notification(f"🍺 found {init_count_365d} initial 365d downloads for {formula_name}")        
+    logger.info(f"🍺 found {init_count_365d} initial 365d downloads for {formula_name}")        
+
+    logger.info("🚀 starting monitoring loop...")
+    while True:
+        #
+        # find new stargazers
+        #
+        logger.debug("checking for new stargazers...")
+        left_join_query = """
+        SELECT c.login
+        FROM github.activity.repo_stargazers c
+        LEFT JOIN mvw_github_repo_stargazers mvw
+        ON c.login = mvw.login
+        WHERE mvw.login IS NULL
+        AND c.owner = '%s' AND c.repo = '%s'
+        """ % (owner, repo)
+        new_stargazers = stackql.execute(left_join_query)
+        # refresh state
+        stackql.executeStmt("REFRESH MATERIALIZED VIEW mvw_github_repo_stargazers")
+        # get overall total stargazers
+        total_stargazers = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_stargazers")[0]['count']
+        logger.debug(f"total stargazers : {total_stargazers}")
+        # send notification
+        if new_stargazers:
+            message = f"⭐ {len(new_stargazers)} new stargazers for {owner}/{repo} ({', '.join([stargazer['login'] for stargazer in new_stargazers])})\n\ntotal stargazers : {total_stargazers}"
+            send_notification(message, slack_icon_emoji=':star:')
+        else: 
+            logger.debug("no new stargazers found")
+
+        #
+        # find repo watchers
+        #        
+        logger.debug("checking for new repo watchers...")
+        left_join_query = """
+        SELECT c.login
+        FROM github.activity.repo_watchers c
+        LEFT JOIN mvw_github_repo_watchers mvw
+        ON c.login = mvw.login
+        WHERE mvw.login IS NULL
+        AND c.owner = '%s' AND c.repo = '%s'
+        """ % (owner, repo)
+        new_repo_watchers = stackql.execute(left_join_query)
+        # refresh state
+        stackql.executeStmt("REFRESH MATERIALIZED VIEW mvw_github_repo_watchers")
+        # get new total repo watchers
+        total_repo_watchers = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_watchers")[0]['count']
+        logger.debug(f"total repo watchers : {total_repo_watchers}")
+        # send notification
+        if new_stargazers:
+            message = f"👀 {len(new_repo_watchers)} new repo watchers for {owner}/{repo} ({', '.join([watcher['login'] for watcher in new_repo_watchers])})\n\ntotal repo watchers : {total_repo_watchers}"
+            send_notification(message, slack_icon_emoji=':eye:')
+        else: 
+            logger.debug("no new repo watchers found")
+
+        #
+        # find new repo forks
+        #
+        logger.debug("checking for new repo forks...")
+        left_join_query = """
+        SELECT json_extract(c.owner, '$.login') as login
+        FROM github.repos.forks c
+        LEFT JOIN mvw_github_repo_forks mvw
+        ON json_extract(c.owner, '$.login') = mvw.login
+        WHERE mvw.login IS NULL
+        AND c.owner = '%s' AND c.repo = '%s'       
+        """ % (owner, repo)
+        new_repo_forks = stackql.execute(left_join_query)
+        # refresh state
+        stackql.executeStmt("REFRESH MATERIALIZED VIEW mvw_github_repo_forks")
+        # get new total repo forks
+        total_repo_forks = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_forks")[0]['count']
+        logger.debug(f"total repo forks : {total_repo_forks}")
+        # send notification
+        if new_repo_forks:
+            message = f"🍴 {len(new_repo_forks)} new repo forks for {owner}/{repo} ({', '.join([fork['login'] for fork in new_repo_forks])})\n\ntotal repo forks : {total_repo_forks}"
+            send_notification(message, slack_icon_emoji=':fork_and_knife:')
+        else:
+            logger.debug("no new repo forks found")
+
+        #
+        # find new repo issues
+        #
+        logger.debug("checking for new repo issues...")
+        left_join_query = """
+        SELECT c.id, json_extract(c.user, '$.login') as reporter
+        FROM github.issues.issues c
+        LEFT JOIN mvw_github_repo_issues mvw
+        ON c.id = mvw.id
+        WHERE mvw.id IS NULL
+        AND c.owner = '%s' AND c.repo = '%s'
+        """ % (owner, repo)
+        new_repo_issues = stackql.execute(left_join_query)
+        # refresh state
+        stackql.executeStmt("REFRESH MATERIALIZED VIEW mvw_github_repo_issues")
+        # get new total repo issues
+        total_repo_issues = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_repo_issues")[0]['count']
+        logger.debug(f"total repo issues : {total_repo_issues}")
+        # send notification
+        if new_repo_issues:
+            reporters = ', '.join([issue['reporter'] for issue in new_repo_issues])
+            message = f"📊 {len(new_repo_issues)} new repo issues for {owner}/{repo} (raised by {reporters})\n\ntotal repo issues : {total_repo_issues}"
+            send_notification(message, slack_icon_emoji=':chart_with_upwards_trend:')
+        else:
+            logger.debug("no new repo issues found")
+
+        #
+        # find new org followers
+        #
+        logger.debug("checking for new org followers...")
+        left_join_query = """
+        SELECT c.login
+        FROM github.users.followers c
+        LEFT JOIN mvw_github_org_followers mvw
+        ON c.login = mvw.login
+        WHERE mvw.login IS NULL
+        AND c.username = '%s'
+        """ % (owner)
+        new_org_followers = stackql.execute(left_join_query)
+        # refresh state
+        stackql.executeStmt("REFRESH MATERIALIZED VIEW mvw_github_org_followers")
+        # get new total org followers
+        total_org_followers = stackql.execute("SELECT COUNT(*) as count FROM mvw_github_org_followers")[0]['count']
+        logger.debug(f"total org followers : {total_org_followers}")
+        # send notification
+        if new_org_followers:
+            message = f"🙋 {len(new_org_followers)} new org followers for {owner}\n\ntotal org followers : {total_org_followers}"
+            send_notification(message, slack_icon_emoji=':busts_in_silhouette:')
+        else:
+            logger.debug("no new org followers found")
+
+        #
+        # find new release asset downloads
+        #
+        logger.debug("checking for new release asset downloads...")
+        # create temp view for current downloads
+        current_query = """
+        CREATE OR REPLACE MATERIALIZED VIEW tmp_current_github_release_asset_downloads AS 
+        SELECT
+        SPLIT_PART(JSON_EXTRACT(json_each.value, '$.browser_download_url'), '/', -1) as asset_name,
+        SUM(JSON_EXTRACT(json_each.value, '$.download_count')) as download_count
+        FROM
+        github.repos.releases, json_each(assets)
+        WHERE owner = '%s' AND repo = '%s'
+        GROUP BY asset_name
+        """ % (owner, repo)
+        stackql.executeStmt(current_query)
+        # find the difference in downloads
+        diff_query = """
+        SELECT c.asset_name,
+        (c.download_count - mvw.download_count) as new_downloads
+        FROM tmp_current_github_release_asset_downloads c
+        INNER JOIN mvw_github_release_asset_downloads mvw
+        ON c.asset_name = mvw.asset_name
+        """
+        total_new_downloads_query = """
+        SELECT SUM(new_downloads) FROM (
+        %s) t
+        """ % diff_query
+        total_new_downloads = stackql.execute(total_new_downloads_query)
+        logger.debug(f"total new downloads : {total_new_downloads[0]['new_downloads']}")
+        if int(total_new_downloads[0]['new_downloads']) > 0:
+            # we have some new downloads
+            asset_downloads = stackql.execute(diff_query)
+            for asset in asset_downloads:
+                if int(asset['new_downloads']) > 0:
+                    send_notification(f"📦 new downloads for {asset['asset_name']}: {asset['new_downloads']}", slack_icon_emoji=':package:')
+        # refresh state
+        stackql.executeStmt("REFRESH MATERIALIZED VIEW mvw_github_release_asset_downloads")
+
+        #
+        # find new homebrew downloads
+        #
+        logger.debug("checking for new homebrew downloads...")
+        # create temporary materialized view for current downloads
+        current_query = """
+        CREATE OR REPLACE MATERIALIZED VIEW tmp_current_homebrew_downloads AS 
+        SELECT
+        JSON_EXTRACT(analytics, '$.install.30d.%s') as installs_30d,
+        JSON_EXTRACT(analytics, '$.install.90d.%s') as installs_90d,
+        JSON_EXTRACT(analytics, '$.install.365d.%s') as installs_365d
+        FROM homebrew.formula.formula
+        WHERE formula_name = '%s'
+        """ % (formula_name, formula_name, formula_name, formula_name)
+        stackql.executeStmt(current_query)
+
+        # find new downloads
+        new_downloads_query = """
+        SELECT 
+        max((c.installs_30d - mvw.installs_30d),(c.installs_90d - mvw.installs_90d),(c.installs_365d - mvw.installs_365d)) as new_downloads
+        FROM tmp_current_homebrew_downloads c
+        JOIN mvw_homebrew_downloads mvw
+        ON 1=1
+        """
+        new_downloads = stackql.execute(new_downloads_query)
+        logger.debug(f"new homebrew downloads : {new_downloads[0]['new_downloads']}")
+        if int(new_downloads[0]['new_downloads']) > 0:
+                message = f"🍺 new homebrew downloads for {formula_name} : {new_downloads[0]['new_downloads']}"
+                send_notification(message, slack_icon_emoji=':beer:')
+        else:
+            logger.debug("no new homebrew downloads found")
+        # refresh state
+        stackql.executeStmt("REFRESH MATERIALIZED VIEW mvw_homebrew_downloads")
+
+        # sleep for sleep_interval secs
+        logger.debug(f"sleeping for {sleep_interval} secs...")
+        time.sleep(int(sleep_interval))
 
 if __name__ == '__main__':
     main()
